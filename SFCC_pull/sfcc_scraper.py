5ededcba9f6158586c55e21a3daff2e7143514a5
alex-miyake <alexandermiyake@btinternet.com>
2026-03-24 14:06:42 +0000

    add gitignore file

--- SFCC_pull/sfcc_scraper.py
+++ SFCC_pull/sfcc_scraper.py
@@ -1,14 +1,2 @@
 
 
-
-
-# Parse data from 1st tab 
-
-
-# Generate T&Cs
-    #  sample name (from LRP samples tracker), if not then just complimentary gift. 
-
-# Populate T&Cs
-    # Dates
-    # Promo type
-    # T&C (generated earlier)
