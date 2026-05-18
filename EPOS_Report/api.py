5380ca9f1fe004b3b1635cc1db6d68eafa35e7ba
alex-miyake <alexandermiyake@btinternet.com>
2026-05-18 17:17:09 +0100

    update onedrive path

--- EPOS_Report/api.py
+++ EPOS_Report/api.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import os
 
 def connect_test():
-    file_path = r"C:\Users\alex.miyake\OneDrive - L'Oréal\Documents\EPOS PBI Alex .xlsx"
+    file_path = os.getenv('filename')
+    print("got .env variable")
     df = pd.read_excel(file_path)
+    print("opened file")
     print(df.head())
     return
--- EPOS_Report/main.py
+++ EPOS_Report/main.py
@@ -1,9 +1,11 @@
 from report import build_report
 from email import send_report
 from api import connect_test
+from dotenv import load_dotenv
 
 if __name__=="__main__":
     print("script starting")
+    load_dotenv()
     connect_test()
     
     # payload, title = build_report()
